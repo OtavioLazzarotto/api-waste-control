0e3de454ecf6fa6e10420b896c2af7df29ed6a1d
Otavin96 <otaviolazzarotto@icloud.com>
2026-06-08 22:43:55 -0300

    update local api

--- frontend/src/helpers/api.ts
+++ frontend/src/helpers/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://api-orders-payment-inventory-production.up.railway.app",
 });
 
 // Injeta o token em todas as requisições
